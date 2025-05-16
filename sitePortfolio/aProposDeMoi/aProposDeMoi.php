aa944ba8ac707b41742789c4cde14337bf938e0d
Raphael MATHERET <raphael.matheret@etu.univ-grenoble-alpes.fr>
2025-05-16 03:02:36 +0200

    Avancement site

---
+++ sitePortfolio/aProposDeMoi/aProposDeMoi.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Portfolio</title>
+    <link rel="icon" type="image/x-icon" href="/icone.jpeg">
+    <link rel="stylesheet" href="/sitePortfolio/aProposDeMoi/aProposDeMoi.css">
+    <link rel="stylesheet" href="/sitePortfolio/headerAndFooter/headerAndFooter.css">
+</head>
+
+<body>
+<?php include("../headerAndFooter/header.php") ?>
+
+<main>
+    <div id="presentation" class="partie">
+        <div id="presentation-text">
+            <h1 class="titre">Présentation</h1>
+            <p>Bonjour, je m'appelle Raphaël MATHERET, j'ai 18 ans. Je suis passioné d'informatique et de jeux vidéo
+                depuis tout petit. J'ai commencé le développement asser jeune pour automatiser un train en légo, puis
+                j'ai continué avec des projets de modifications de jeux, très principalement Minecraft. J'aime aussi
+                m'occuper des enfants, j'ai donc passé la première partie du BAFA et fais un job d'été en périscolaire
+                dans une école près de chez moi. J'ai aussi réalisé un petit film amateur avec des jeunes du centre
+                AÉRÉ de mon village à l'occasion d'halloween.
+            </p>
+        </div>
+
+        <img src="/sitePortfolio/aProposDeMoi/photo.jpg" alt="photo Présentation">
+    </div>
+
+    <div id="mesExperiences" class="partie">
+        <h1 class="titre">Mes Experiences</h1>
+        <ul>
+            <li>
+                <img src="/sitePortfolio/aProposDeMoi/experiencesPrefix.png" alt="Éxpériences préfix">
+                <div class="exempleText">
+                    <h2>Weatheria</h2>
+                    <p>Je fais partis de ce projet de serveur Minecraft en tant que développeur. On est une dizaine
+                        avec une hierachie bien définie pour facilité l'avancement du projet. On a des réunions
+                        hebdomadaire avec notre responsable de pôle et une réunion mensuel pour faire un point interpôles.
+                    </p>
+                </div>
+            </li>
+            <li>
+                <img src="/sitePortfolio/aProposDeMoi/experiencesPrefix.png" alt="Éxpériences préfix">
+                <div class="exempleText">
+                    <h2>Un titre d'éxemple</h2>
+                    <p>Je sais pas un exemple</p>
+                </div>
+            </li>
+            <li>
+                <img src="/sitePortfolio/aProposDeMoi/experiencesPrefix.png" alt="Éxpériences préfix">
+                <div class="exempleText">
+                    <h2>Un titre d'éxemple</h2>
+                    <p>Je sais pas un exemple</p>
+                </div>
+            </li>
+            <li>
+                <img src="/sitePortfolio/aProposDeMoi/experiencesPrefix.png" alt="Éxpériences préfix">
+                <div class="exempleText">
+                    <h2>Un titre d'éxemple</h2>
+                    <p>Je sais pas un exemple</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+</main>
+</body>
+</html>
---
+++ sitePortfolio/headerAndFooter/header.php
@@ -0,0 +1,9 @@
+
+<header>
+    <div id="header">
+        <a href="/">Raphaël MATHERET</a>
+        <a href="/portfolio">À propos de moi</a>
+        <a href="/portfolio/projets">Projets</a>
+        <a href="/portfolio/contact">Contact</a>
+    </div>
+</header>
