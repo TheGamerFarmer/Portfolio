da27da87c41a63ad77dcd59d6481e9ada714c237
Raphael MATHERET <raphael.matheret@etu.univ-grenoble-alpes.fr>
2025-06-08 06:49:46 +0200

    Fin text hardcodé

--- sitePortfolio/aProposDeMoi/aProposDeMoi.php
+++ sitePortfolio/aProposDeMoi/aProposDeMoi.php
@@ -19,12 +19,13 @@
 
         <div id="presentation-text">
             <h1 class="titre">Présentation</h1>
-            <p>Bonjour, je m'appelle Raphaël MATHERET, j'ai 18 ans.<br>Je suis passionné d'informatique et de jeux vidéo
-                depuis tout petit. J'ai commencé le développement assez jeune pour automatiser un train en légo, puis
-                j'ai continué avec des projets de modifications de jeux, très principalement Minecraft.<br>J'aime aussi
-                m'occuper des enfants, j'ai donc passé la première partie du BAFA et fais un job d'été en périscolaire
-                dans une école près de chez moi. J'ai aussi réalisé un petit film amateur avec des jeunes du centre
-                AÉRÉ de mon village à l'occasion d'Halloween.
+            <p>Bonjour, je m'appelle Raphaël MATHERET, j'ai 18 ans.<br>
+                Je suis passionné d'informatique et de jeux vidéo depuis tout petit. J'ai commencé le développement
+                assez jeune pour automatiser un train en Lego, puis j'ai continué avec des projets de modification de
+                jeux, principalement Minecraft.<br>
+                J'aime aussi m'occuper des enfants : j'ai donc passé la première partie du BAFA et effectué un job d'été
+                en périscolaire dans une école près de chez moi. J'ai également réalisé un petit film amateur avec des
+                jeunes du centre aéré de mon village à l'occasion d'Halloween.
             </p>
         </div>
 
@@ -42,31 +43,34 @@
                 <img src="/sitePortfolio/aProposDeMoi/experiencesPrefix.png" alt="Éxpériences préfix">
                 <div class="exempleText">
                     <h2>Weatheria</h2>
-                    <p>Je fais parti de ce projet de serveur Minecraft en tant que développeur. On est une dizaine
-                        avec une hiérarchie bien définie pour faciliter l'avancement du projet. On a des réunions
-                        hebdomadaires avec notre responsable de pôle et une réunion mensuel pour faire un point interpôles.
+                    <p>
+
+                        Je fais partie de ce projet de serveur Minecraft en tant que développeur. Nous sommes une
+                        dizaine, avec une hiérarchie bien définie pour faciliter l'avancement du projet.
+                        Nous organisons des réunions hebdomadaires avec notre responsable de pôle, ainsi qu'une réunion
+                        mensuelle pour faire un point entre les pôles.
                     </p>
                 </div>
             </li>
             <li>
                 <img src="/sitePortfolio/aProposDeMoi/experiencesPrefix.png" alt="Éxpériences préfix">
                 <div class="exempleText">
-                    <h2>Un titre d'exemple</h2>
-                    <p>Je ne sais pas un exemple</p>
+                    <h2>Périscolaire</h2>
+                    <p>Comme dit plus haut, j’ai eu l’occasion, lors de mon stage BAFA, de passer un mois en périscolaire
+                        dans l’école primaire de Pinet. Je remplaçais une animatrice malade. J’aidais principalement à
+                        gérer la pause méridienne et le repas du midi. J’ai également participé à l’organisation de la
+                        kermesse de fin d’année, notamment en créant un diaporama des meilleures photos et vidéos de l’année.</p>
                 </div>
             </li>
             <li>
                 <img src="/sitePortfolio/aProposDeMoi/experiencesPrefix.png" alt="Éxpériences préfix">
                 <div class="exempleText">
-                    <h2>Un titre d'exemple</h2>
-                    <p>Je ne sais pas un exemple</p>
-                </div>
-            </li>
-            <li>
-                <img src="/sitePortfolio/aProposDeMoi/experiencesPrefix.png" alt="Éxpériences préfix">
-                <div class="exempleText">
-                    <h2>Un titre d'exemple</h2>
-                    <p>Je ne sais pas un exemple</p>
+                    <h2>Youtube</h2>
+                    <p>J’ai tenu pendant quelque temps une chaîne YouTube :
+                        <a href="https://www.youtube.com/@thegamerfarmer">TheGamerFarmer</a>.
+                        Je voulais m’essayer au montage vidéo et m’entraîner à m’exprimer à l’oral. Ça n’a pas été une
+                        franche réussite, mais cela m’a permis de me lancer dans un projet, d’apprendre par moi-même,
+                        ce que j’ai refait un peu plus tard en reprenant le développement.</p>
                 </div>
             </li>
         </ul>
